c72209257e5d52e92e0dfccf5358d10873639fb3
dNaszta <david.nasztanovics@stylersonline.com>
2020-11-04 11:31:15 +0100

    refactor references

--- Ch01-io-interfaces/L01-interfaces/example/main.go
+++ Ch01-io-interfaces/L01-interfaces/example/main.go
@@ -10,14 +10,14 @@ func main()  {
 	in := bytes.NewReader([]byte("example"))
 	out := &bytes.Buffer{}
 	fmt.Print("stdout on Copy = ")
-	if err := L01_interfaces.Copy(in, out); err != nil {
+	if err := interfaces.Copy(in, out); err != nil {
 		panic(err)
 	}
 
 	fmt.Println("out bytes buffer = ", out.String())
 
 	fmt.Print("stdout on PipeExample = ")
-	if err := L01_interfaces.PipeExample(); err != nil {
+	if err := interfaces.PipeExample(); err != nil {
 		panic(err)
 	}
 }
