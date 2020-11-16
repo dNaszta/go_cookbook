ae3829efb74215426f0646efd0f1e554d941fb8e
dNaszta <david.nasztanovics@stylersonline.com>
2020-11-16 18:31:23 +0100

    csv example code

---
+++ Ch01-io-interfaces/L04-csvformat/example/go.mod
@@ -0,0 +1,5 @@
+module github.com/dNaszta/go_cookbook/Ch01-io-interfaces/L04-csvformat/example
+
+go 1.14
+
+require github.com/dNaszta/go_cookbook/Ch01-io-interfaces/L04-csvformat/csvformat v0.0.0-20201116172245-5348deb842c7
---
+++ Ch01-io-interfaces/L04-csvformat/example/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dNaszta/go_cookbook/Ch01-io-interfaces/L04-csvformat/csvformat"
+)
+
+func main() {
+	if err := csvformat.AddMoviesFromText(); err != nil {
+		panic(err)
+	}
+
+	if err := csvformat.WriteCSVOutput(); err != nil {
+		panic(err)
+	}
+
+	buffer, err := csvformat.WriteCSVBuffer()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Buffer = ", buffer.String())
+}
