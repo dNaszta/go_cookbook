6027e470b4e43c26978c8558c8d22c1eae576f71
dNaszta <david.nasztanovics@stylersonline.com>
2020-11-29 13:55:13 +0100

    templates example

---
+++ Ch01-io-interfaces/L06-templates/example/go.mod
@@ -0,0 +1,5 @@
+module github.com/dNaszta/go_cookbook/Ch01-io-interfaces/L06-templates/example
+
+go 1.14
+
+require github.com/dNaszta/go_cookbook/Ch01-io-interfaces/L06-templates/templates v0.0.0-20201129124421-128ff7129eda
---
+++ Ch01-io-interfaces/L06-templates/example/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/dNaszta/go_cookbook/Ch01-io-interfaces/L06-templates/templates"
+
+func main() {
+	if err := templates.RunTemplate(); err != nil {
+		panic(err)
+	}
+
+	if err := templates.InitTemplates(); err != nil {
+		panic(err)
+	}
+
+	if err := templates.HTMLDifferences(); err != nil {
+		panic(err)
+	}
+}
